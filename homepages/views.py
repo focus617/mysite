862b3f62b928e535c306ce267158897eff1bc145
Zhiyong Xu <13980668829@163.com>
2018-12-20 18:07:16 +0800

    Add homepages:html following the <<Head First HTML and CSS>>.

--- homepages/views.py
+++ homepages/views.py
@@ -14,3 +14,19 @@ def date_time(request, offset=0):
         raise Http404()
     now = datetime.datetime.now() + datetime.timedelta(hours=offset_)
     return render(request, 'homepages/datetime.html', {"current_datetime": now})
+
+
+def firstLoughe(request):
+    return render(request, 'homepages/firstLoughe.html')
+
+
+def newLounge(request):
+    return render(request, 'homepages/newLounge.html')
+
+
+def myFirstJavaScript(request):
+    return render(request, 'homepages/myFirstJavaScript.html')
+
+
+def json(request):
+    return render(request, 'homepages/json/json.html')
